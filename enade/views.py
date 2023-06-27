3f3523000a1a88370c8f639fa3420fd442dbc697
Igor Dias <igorverissimo02@hotmail.com>
2023-06-27 17:53:22 -0300

    adicionando links inciais

--- enade/views.py
+++ enade/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def home(request):
+    return render(request, 'home.html')
+
+def criar_simulado(request):
+    return render(request, 'criar_simulado.html')
+
+def buscar_questoes(request):
+    return render(request, 'buscar_questoes.html')
+
