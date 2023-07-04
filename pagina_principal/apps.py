f64c5dc3432b27de1126f248e459d41662f95e67
Igor Dias <igorverissimo02@hotmail.com>
2023-07-04 14:00:55 -0300

    recriação do projeto

--- pagina_principal/apps.py
+++ pagina_principal/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class EnadeConfig(AppConfig):
+class PaginaPrincipalConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'enade'
+    name = 'pagina_principal'
