50a795c2fcd4ee0deab7b9a72e419ceb9adf7fad
Igor Dias <igorverissimo02@hotmail.com>
2023-07-21 14:10:15 -0300

    fix: removing unnecessary fields and adding the user field in the question model and simulated

--- pagina_principal/models.py
+++ pagina_principal/models.py
@@ -1,35 +1,43 @@
 from django.db import models
+from usuarios.models import Usuario
 
 
 class Assunto(models.Model):
-    id = models.AutoField(primary_key = True)
-    nome_assunto = models.CharField(max_length = 150 , unique =True )
+    id = models.AutoField(primary_key=True)
+    nome_assunto = models.CharField(max_length=150, unique=True)
 
     def __str__(self):
         return self.nome_assunto
 
+
 class Questao(models.Model):
 
-    id = models.AutoField(primary_key = True)
+    id = models.AutoField(primary_key=True)
     assuntos = models.ManyToManyField(Assunto)
-    n_questao = models.IntegerField(null = False, default=0)
-    tipo_questao = models.CharField(max_length=120, null = False, default='none')
-    curso = models.TextField(null = False, default = "Não Cadastrado")
+    curso = models.TextField(null=False, default="Não Cadastrado")
     pergunta = models.TextField(default='none')    
-    alternativa_a = models.TextField(null = False, default='none')
-    alternativa_b = models.TextField(null = False, default='none')
-    alternativa_c = models.TextField(null = False, default='none')
-    alternativa_d = models.TextField(null = False, default='none')
-    alternativa_e = models.TextField(null = False, default='none')
-    alternativa_correta = models.CharField(max_length = 1, null = False, default='0')
-    
+    alternativa_a = models.TextField(null=False, default='none')
+    alternativa_b = models.TextField(null=False, default='none')
+    alternativa_c = models.TextField(null=False, default='none')
+    alternativa_d = models.TextField(null=False, default='none')
+    alternativa_e = models.TextField(null=False, default='none')
+    alternativa_correta = models.CharField(max_length=1,
+                                           null=False, default='0')
+    autor = models.ForeignKey(
+        Usuario, on_delete=models.SET_NULL, null=True
+    )
+
     def __str__(self):
         return str(self.id)
-    
+
+
 class Simulado(models.Model):
-    id = models.AutoField(primary_key = True)
+    id = models.AutoField(primary_key=True)
     titulo = models.CharField(max_length=300)
     questoes = models.ManyToManyField(Questao)
+    autor = models.ForeignKey(
+        Usuario, on_delete=models.SET_NULL, null=True
+    )
 
     def __str__(self):
         return self.titulo
--- pagina_principal/views.py
+++ pagina_principal/views.py
@@ -4,61 +4,68 @@ from .models import Questao, Assunto, Simulado
 import json
 from random import sample
 
+
 def home(request):
     return render(request, 'pagina_principal/home.html')
+
 
 def lista_questoes(request):
     questoes = Questao.objects.all()
     assuntos = Assunto.objects.all()
 
     if request.method == 'POST':
-        assuntos_ids = request.POST.getlist('assuntos')
-        tipo_questao = request.POST['tipo_questao']
-        
+        assuntos_ids = request.POST.getlist('assuntos')       
         if len(assuntos_ids) != 0:
             questoes = questoes.filter(assuntos__id__in=assuntos_ids)
 
-        if tipo_questao != '':
-            questoes = questoes.filter(tipo_questao=tipo_questao)
+    return render(request, 'pagina_principal/lista_questoes.html',
+                  {'questoes': questoes, 'assuntos': assuntos})
 
-    return render(request, 'pagina_principal/lista_questoes.html', {'questoes':questoes, 'assuntos':assuntos})
 
 def simulado(request, simulado_id):
 
-    simulado =  get_object_or_404(Simulado, id = simulado_id)
+    simulado = get_object_or_404(Simulado, id=simulado_id)
     questoes = simulado.questoes.all()
     if request.method == 'POST':
         dados = json.loads(request.body)
         resposta = []
         for questao in dados: 
-            dados_questao = get_object_or_404(Questao, id = dados[questao]['id_questao'])
+            dados_questao = get_object_or_404(Questao, 
+                                              id=dados[questao]['id_questao'])
 
             if dados_questao.alternativa_correta == dados[questao]['resposta']:
-
-                resposta.append({'status':'Resposta Correta', 'id_questao':dados[questao]['id_questao']})
+                resposta.append({'status': 'Resposta Correta',
+                                 'id_questao': dados[questao]['id_questao']})
             else:
-                resposta.append({'status':'Resposta Errada', 'id_questao':dados[questao]['id_questao']})
-        return HttpResponse(json.dumps({'resultado':resposta}))
-        
+                resposta.append({'status': 'Resposta Errada',
+                                 'id_questao': dados[questao]['id_questao']})
+        return HttpResponse(json.dumps({'resultado': resposta}))
     
-    return render(request,'pagina_principal/simulado.html', {'questoes':questoes, 'simulado':simulado})
+    return render(request, 'pagina_principal/simulado.html',
+                  {'questoes': questoes, 'simulado': simulado})
+
 
 def cadastrar_questao(request):
     assuntos = Assunto.objects.all()
     if request.method == 'POST':
         dados = json.loads(request.body)
         assuntos_ids = dados['assuntos']
-        questao = Questao.objects.create( pergunta = dados['pergunta'], curso = dados['nome_curso'], tipo_questao = dados['tipo_questao'],
-                n_questao = dados['n_questao'], alternativa_a = dados['alternativa_a'], alternativa_b = dados['alternativa_b'],
-                alternativa_c = dados['alternativa_c'], alternativa_d = dados['alternativa_d'], alternativa_e = dados['alternativa_e'],
-                alternativa_correta = dados['alternativa_correta'] )
+        questao = Questao.objects.create(pergunta=dados['pergunta'], 
+                                         curso=dados['nome_curso'],  
+                                         alternativa_a=dados['alternativa_a'], 
+                                         alternativa_b=dados['alternativa_b'],
+                                         alternativa_c=dados['alternativa_c'],
+                                         alternativa_d=dados['alternativa_d'],
+                                         alternativa_e=dados['alternativa_e'],
+                                         alternativa_correta=dados['alternativa_correta']  # noqa: E501
+                                         )
 
         assuntos_aux = assuntos_ids.copy()
         for assunto_pagina in assuntos_aux:
             if 'novo_' in assunto_pagina:
                 assuntos_ids.remove(assunto_pagina)
                 assunto_pagina = assunto_pagina.replace('novo_', '')
-                assunto_novo = Assunto.objects.create(nome_assunto=assunto_pagina)         
+                assunto_novo = Assunto.objects.create(nome_assunto=assunto_pagina)  # noqa: E501
                 questao.assuntos.add(assunto_novo)
 
         assuntos_ids = list(map(int, assuntos_ids))
@@ -66,35 +73,38 @@ def cadastrar_questao(request):
         for assunto in assuntos: 
             questao.assuntos.add(assunto)
 
-    return render(request, 'pagina_principal/cadastrar_questao.html', {'assuntos':assuntos})
+    return render(request, 'pagina_principal/cadastrar_questao.html',
+                  {'assuntos': assuntos})
+
 
 def editar_questao(request, questao_id):
     
-    questao = get_object_or_404(Questao, id = questao_id)
+    questao = get_object_or_404(Questao, id=questao_id)
 
-    if request.method == 'POST':    
-            dados = json.loads(request.body)
-            questao.pergunta = dados['pergunta']
-            questao.curso = dados['nome_curso']
-            questao.tipo_questao = dados['tipo_questao']
-            questao.n_questao = dados['n_questao']
-            questao.alternativa_a = dados['alternativa_a']
-            questao.alternativa_b = dados['alternativa_b']
-            questao.alternativa_c = dados['alternativa_c']
-            questao.alternativa_d = dados['alternativa_d']
-            questao.alternativa_e = dados['alternativa_e']
-            questao.alternativa_correta = dados['alternativa_correta']
+    if request.method == 'POST':
+        dados = json.loads(request.body)
+        questao.pergunta = dados['pergunta']
+        questao.curso = dados['nome_curso']
+        questao.alternativa_a = dados['alternativa_a']
+        questao.alternativa_b = dados['alternativa_b']
+        questao.alternativa_c = dados['alternativa_c']
+        questao.alternativa_d = dados['alternativa_d']
+        questao.alternativa_e = dados['alternativa_e']
+        questao.alternativa_correta = dados['alternativa_correta']
 
-            questao.save()
+        questao.save()
+        
+        return HttpResponse('1')
             
-            return HttpResponse('1')
-            
-    return render(request, 'pagina_principal/editar_questao.html', {'questao': questao})
+    return render(request, 'pagina_principal/editar_questao.html', 
+                  {'questao': questao})
+
 
 def criar_simulado(request):
-    #TODO Verificar se todos os campos foram preenchidos para evitar erros
-    #TODO adicionar requerid no form
-    #TODO verificar se a quantiade de questões disponiveis é suficiente para criar o simulado ex: foi pedido para criar um simulado 
+    # TODO Verificar se todos os campos foram preenchidos para evitar erros
+    # TODO adicionar requerid no form
+    # TODO verificar se a quantiade de questões disponiveis é suficiente
+    # para criar o simulado ex: foi pedido para criar um simulado
     # com 10 questões, mas só tem 5 questões cadastradas
 
     assuntos = Assunto.objects.all()
@@ -108,14 +118,16 @@ def criar_simulado(request):
         if len(assuntos_ids) != 0:
             questoes = questoes.filter(assuntos__id__in=assuntos_ids).distinct()
         
-        indices_para_sorteio = list(range(0,len(questoes)))
+        indices_para_sorteio = list(range(0, len(questoes)))
         indices_escolhidos = sample(indices_para_sorteio, qtd_questoes)
         simulado = Simulado.objects.create(titulo=titulo)
         for indice in indices_escolhidos:
             simulado.questoes.add(questoes[indice])
         return redirect('lista_simulados')
-    return render(request, 'pagina_principal/criar_simulado.html', {'assuntos':assuntos})
-    
+    return render(request, 'pagina_principal/criar_simulado.html', 
+                  {'assuntos': assuntos})
+
+
 def criar_simulado_manualmente(request):
     questoes = Questao.objects.all()
     assuntos = Assunto.objects.all()
@@ -125,10 +137,13 @@ def criar_simulado_manualmente(request):
         questoes_escolhidas = questoes.filter(id__in=id_questoes_escolhidas)
         simulado = Simulado.objects.create(titulo=titulo)
         simulado.questoes.set(questoes_escolhidas)
-    return render(request, 'pagina_principal/criar_simulado_manualmente.html', {'questoes': questoes, 'assuntos':assuntos})
+    return render(request, 'pagina_principal/criar_simulado_manualmente.html',
+                  {'questoes': questoes, 'assuntos': assuntos})
+
 
 def lista_simulados(request):
     simulados = Simulado.objects.all()
-    return render(request, 'pagina_principal/lista_simulados.html', {'simulados':simulados})
+    return render(request, 'pagina_principal/lista_simulados.html',
+                  {'simulados': simulados})
 
 
