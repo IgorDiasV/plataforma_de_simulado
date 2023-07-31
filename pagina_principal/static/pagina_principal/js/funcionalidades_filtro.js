a5091852fc2ab46a76914d5d2f6ad4b53b1bb131
Igor Dias <igorverissimo02@hotmail.com>
2023-07-31 16:09:31 -0300

    refactor: putting the script in a separate file

---
+++ pagina_principal/static/pagina_principal/js/funcionalidades_filtro.js
@@ -0,0 +1,30 @@
+function  mudar_visibilidade_filtro(id_campo_filtro, id_seta)
+{
+    let lista_itens = document.getElementById(id_campo_filtro);
+    let seta =  document.getElementById(id_seta)
+
+    if (lista_itens.style.display == 'none' || lista_itens.style.display == '' ) {
+        seta.classList.remove('fa-chevron-down')
+        seta.classList.add('fa-chevron-up')
+        lista_itens.style.display = 'flex';
+    } else {
+        seta.classList.remove('fa-chevron-up')
+        seta.classList.add('fa-chevron-down')
+        lista_itens.style.display = 'none';
+    }
+}
+
+function filtrar(id_busca, id_item){
+    let busca = document.getElementById(id_busca);
+    let itens_do_filtro = document.querySelectorAll(id_item);
+    let filtro = busca.value.toLowerCase();
+    for (let item of itens_do_filtro) {
+        let nome_item = item.querySelector('label').textContent.toLowerCase();
+        if (nome_item.includes(filtro)){
+            item.style.display = 'block'
+        }else{
+            item.style.display = 'none'
+        }
+        
+    }
+}
