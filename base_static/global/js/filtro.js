ac3f23434e2a5a8392d26083406ece8446947637
Igor Dias <igorverissimo02@hotmail.com>
2023-08-09 08:21:08 -0300

    refactor: removendo um pouco da repetição de códig

---
+++ base_static/global/js/filtro.js
@@ -0,0 +1,25 @@
+function get_values_checked_filter(form, input_name){
+  const query = "[name=" + input_name + "]:checked"
+  let inputs_checked = form.querySelectorAll(query)
+  let ids_filtro_checked = []
+  inputs_checked.forEach((input_checked) => {
+    ids_filtro_checked.push(input_checked.value)
+  })
+  return ids_filtro_checked
+}
+
+function get_filtros_escolhidos(){
+    let form = document.getElementById('form_filtro')
+    let ids_filtro_assuntos = get_values_checked_filter(form, 'assuntos')
+    let ids_filtro_anos = get_values_checked_filter(form, 'anos')
+    return [ids_filtro_assuntos, ids_filtro_anos]
+  }
+
+  function filtrar(){
+      
+      let parametros_gerais = parametros_para_redirecionar()     
+      history.replaceState(null, null, window.location.pathname);
+      const url = window.location.href + '?' + parametros_gerais
+      window.location.href = url
+  }
+
