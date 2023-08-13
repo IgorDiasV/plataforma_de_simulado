abd9cc057efbf12ab021fd3a53f170948d61243f
Igor Dias <igorverissimo02@hotmail.com>
2023-08-13 09:40:27 -0300

    feat: added button to display all chosen questions

--- base_static/global/js/utils.js
+++ base_static/global/js/utils.js
@@ -1,9 +1,9 @@
-function mudar_ocultar_questao(id_div_questao, id_seta_questao){
+function mudar_ocultar_questao(id_div_questao, id_seta_questao, tamanho='200px'){
     let div_questao = document.getElementById(id_div_questao);
     let seta_questao = document.getElementById(id_seta_questao);
 
     if (div_questao.style.height == 'auto'){
-      div_questao.style.height = '200px'
+      div_questao.style.height = tamanho
       seta_questao.classList.remove('fa-chevron-up')
       seta_questao.classList.add('fa-chevron-down')
     }else{
