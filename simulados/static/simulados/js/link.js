f54ceca87be6393ca772acc8fe5d504f2318791e
Igor Dias <igorverissimo02@hotmail.com>
2023-08-25 10:46:28 -0300

    reafactor: removed unused function

--- simulados/static/simulados/js/link.js
+++ simulados/static/simulados/js/link.js
@@ -1,30 +1,3 @@
-async function gerar_link(id_simulado, url)
-{
-    
-        var csrf_token = document.querySelector('[name=csrfmiddlewaretoken]').value
-        const configuracao = {
-            method : 'POST',
-            headers:{
-                "Content-Type":'application/json',
-                'X-CSRFToken': csrf_token
-        },
-          body: JSON.stringify({'id_simulado':id_simulado})
-        }
-        
-        const reposta = await fetch(url,configuracao)
-        const link = await reposta.json()        
-                      
-        var inputLink = document.getElementById('link'+id_simulado)
-        inputLink.setAttribute("value", link.link);
-        inputLink.style.display = 'inline-block'
-
-        var botaoGerar = document.getElementById('botaoGerar'+id_simulado)
-        botaoGerar.remove()
-        var botaoCopiar = document.getElementById('botaoCopiar'+id_simulado)
-        botaoCopiar.style.display = 'block'
-  
-}
-
 function copiarLink(id_simulado) {
     let textoCopiado = document.getElementById(id_simulado);
     textoCopiado.select();
